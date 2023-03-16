77a21868b7ea62172eab1a7d22ed5bfe57c6b2a2
MarinaUnraf <marina.henzenn@unraf.edu.ar>
2023-03-16 18:17:41 -0300

    testing functions on stats

--- Assets/Scripts/stats.js
+++ Assets/Scripts/stats.js
@@ -1,3 +1,6 @@
+let fetchedData = [];
+let statistics= [];
+
 function getEventsData(){
     fetch("https://mindhub-xj03.onrender.com/api/amazing")
     .then(response => response.json())
@@ -6,11 +9,17 @@ function getEventsData(){
                 fetchedData = dataApi;
                 /* console.log(fetchedData); */
                 const eventData = fetchedData.events;
+                const currentDate = fetchedData.currentDate;
                 /* console.log(eventData); */
-                const evCapacity = eventData.map(eventCapacity => eventCapacity.capacity);
-                let maxCapacity = Math.max(...evCapacity)
-
-                showTable(maxCapacity)
+               
+                calculateMax(eventData,currentDate);
+                calculePercent(eventData)
+                
+                
+                
+                
+                
+                
               
                 
     } )
@@ -18,8 +27,34 @@ function getEventsData(){
 }
 getEventsData();
 
-
 /* calculate the attendance percent  event */
+    function calculePercent(arrData) {
+    
+        let percents= []
+        arrData.forEach(element => {
+            if(element.assistance){
+                let percent= (element.assistance *100)/ element.capacity;
+                percent= percent.toFixed(2);
+                percents.push(percent)
+            }
+            else if(element.estimate){
+                let percent= (element.estimate *100)/ element.capacity;
+                percent= percent.toFixed(2);
+                percents.push(percent)
+            }
+            
+            
+            
+        });
+        console.log(percents);
+
+        let maxPercent = Math.max(...percents);
+        console.log(maxPercent);
+        let result2 = ""
+        
+
+    }
+
 
 /* calculate  max percent attendance */
 
@@ -27,21 +62,35 @@ getEventsData();
 
 
 /* calculate the event with largest capacity */
-
+function calculateMax(arrData, dateToday) {
+    const evCapacity = arrData.map(eventCapacity => eventCapacity.capacity);
+    let maxCap = Math.max(...evCapacity);
+    
+    let eventMaxCapacity = arrData.filter(event => event.capacity == maxCap && event.date < dateToday)
+    let result = eventMaxCapacity.map(name => name.name) 
+    showtable(result, 0)
+    
+}
 
 
 /* show data in table */
-function showTable(capacity) {
-    let rowContainer = document.getElementById("eventsStatistics");
-     let cellRegister = "";
-    
-        cellRegister += `
-        <td> ....</td>
-        <td> ....</td>
-        <td>${capacity}</td>
+    const showtable = (param1,param2) => {
+        let rowContainer = document.getElementById("eventsStatistics");
+        let register = ''
+        register += ` 
+        <td>...</td>
+        <td>${param2}</td>
+        <td>${param1}</td>
+        
         `;
+        rowContainer.innerHTML = register
 
-    rowContainer.innerHTML = cellRegister
+
+    }
+
     
-    
-}
+   
+
+   
+
+   
